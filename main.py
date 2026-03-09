ee8aa0bb37e8eccd3c607066c204a06086893c6c
nairadi825 <nairadi825@gmail.com>
2026-03-09 19:49:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,19 @@ def multiply(a: float, b: float):
     - JSON object with the result
     """
     return {"result": a * b}
+
+@app.get("/divide/{a}/{b}", status_code=200)
+def divide(a: float, b: float):
+    """
+    Divide one number by another.
+
+    Parameters:
+    - a: Numerator
+    - b: Denominator
+
+    Returns:
+    - JSON object with the result
+    """
+    if b == 0:
+        return {"error": "Divide by zero error. Please enter a non-zero error for the second number"}
+    return {"result": a / b}
